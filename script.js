172b909b0a3024d579ef8ff98fa009ac3d6a6cfe
Ekaterina-987 <77798912+Ekaterina-987@users.noreply.github.com>
2022-10-29 15:32:45 +0300

    hometask-10

--- script.js
+++ script.js
@@ -1,8 +1,51 @@
 const form = document.querySelector(".form");
-form.addEventListener("submit", (event) => {
-  // Предотвращает действие браузера по умолчанию. В данном случае — отправку формы
-  // https://learn.javascript.ru/default-browser-action
-  event.preventDefault();
+const name = document.querySelector("#name");
+const secondName = document.querySelector("#secondName");
+const email = document.querySelector("#email");
+const phone = document.querySelector("#phone");
+const agree = document.querySelector("#agree");
+const button = document.querySelector("#button");
 
-  // Здесь твой код
+form.addEventListener("submit", (event) => {
+    // Предотвращает действие браузера по умолчанию. В данном случае — отправку формы
+    // https://learn.javascript.ru/default-browser-action
+    event.preventDefault();
+
+    // Здесь твой код
+    button.addEventListener("click", () => {
+        fetch('https://polinashneider.space/', {
+            method: "POST",
+            headers: {
+                'Accept': 'aplication/json',
+                'Content-Type': 'aplication/json',
+                'Authorization': 'Bearer: Ekaterina-987'
+            },
+            body: JSON.stringify({
+                "name": "name",
+                "secondName": "secondName",
+                "email": "email",
+                "phone": "phone",
+                "agree": "agree"
+            })
+        });
+
+        function sendRequest() {
+            return new Promise(function(resolve, reject) {
+                    if (fetch = true) {
+                        resolve("Вы успешно зарегистрированы");
+                    } else {
+                        reject("Произошла ошибка");
+                    }
+                })
+                .then((result) => {
+                    alert(result);
+                    document.querySelector(".form").reset()
+                })
+                .catch((error) => {
+                    alert(error);
+                })
+        }
+
+    })
+
 });
